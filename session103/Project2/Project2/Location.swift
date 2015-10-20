00e2039f519e1d6645613e7a4c2a685ed16fe4c1
Alex Telek <alex.telek@shazam.com>
2015-10-20 12:53:32 +0100

    added assets for Session 103-Project2

---
+++ session103/Project2/Project2/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Project2
+//
+//  Created by Alex Telek on 19/10/2015.
+//  Copyright © 2015 King's College London. All rights reserved.
+//
+
+import UIKit
+
+class Location: NSObject {
+
+}
